7b51c331ae108af66353803d8173d4188e0a6c2a
Gh-Story <hg820801350@icloud.com>
2021-12-04 22:09:18 -0800

    new

--- deep/urls.py
+++ deep/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^indexPlus/solvePlus/$', solve_plus),
